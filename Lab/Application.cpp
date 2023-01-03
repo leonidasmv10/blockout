575595bf32a3822368a9a4d173b9f3377eb4c987
Yordy Leonidas MV <yordy.lmv.2000@gmail.com>
2023-01-03 03:27:28 +0100

    small fix

--- Lab/Application.cpp
+++ Lab/Application.cpp
@@ -10,212 +10,236 @@
 
 Application::Application(const std::string& name, const std::string& version)
 {
-    glfwSetErrorCallback([](int code, const char* description)
-    {
-        glfwGetError(&description);
-        std::cerr << "Error " << "0x" << std::hex << code << ':' << description << "\n";
-    });
+	glfwSetErrorCallback([](int code, const char* description)
+		{
+			glfwGetError(&description);
+	std::cerr << "Error " << "0x" << std::hex << code << ':' << description << "\n";
+		});
 
-    // Initialization of glfw.
-    if (!glfwInit())
-    {
-        std::cin.get();
-        return;
-    }
+	// Initialization of glfw.
+	if (!glfwInit())
+	{
+		std::cin.get();
+		return;
+	}
 
-    // Create a window
-    glfwWindowHint(GLFW_RESIZABLE, false);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    window = glfwCreateWindow(width, height, name.c_str(), nullptr, nullptr);
-    if (window == nullptr)
-    {
-        glfwTerminate();
-        std::cin.get();
-        return;
-    }
+	// Create a window
+	glfwWindowHint(GLFW_RESIZABLE, false);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	window = glfwCreateWindow(width, height, name.c_str(), nullptr, nullptr);
+	if (window == nullptr)
+	{
+		glfwTerminate();
+		std::cin.get();
+		return;
+	}
 
-    glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
-    {
-        auto w = static_cast<Application*>(glfwGetWindowUserPointer(window));
-        if (w->key_input) w->key_input(key, scancode, action, mods);
-    });
-    glfwSetWindowUserPointer(window, this);
+	glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
+		{
+			auto w = static_cast<Application*>(glfwGetWindowUserPointer(window));
+	if (w->key_input) w->key_input(key, scancode, action, mods);
+		});
+	glfwSetWindowUserPointer(window, this);
 
-    //Set the OpenGL context
-    glfwMakeContextCurrent(window);
+	//Set the OpenGL context
+	glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        glfwTerminate();
-        return;
-    }
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return;
+	}
 
-    InputCallback();
+	InputCallback();
 
-    // Enable capture of debug output.
-    glEnable(GL_DEBUG_OUTPUT);
-    glDebugMessageCallback(
-        [](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
-           const GLchar* message, const void* userParam)
-        {
-            std::cerr << "GL CALLBACK:" << (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "") <<
-                "type = 0x" << type <<
-                ", severity = 0x" << severity <<
-                ", message =" << message << "\n";
-        }, 0);
+	// Enable capture of debug output.
+   /* glEnable(GL_DEBUG_OUTPUT);
+	glDebugMessageCallback(
+		[](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
+		   const GLchar* message, const void* userParam)
+		{
+			std::cerr << "GL CALLBACK:" << (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "") <<
+				"type = 0x" << type <<
+				", severity = 0x" << severity <<
+				", message =" << message << "\n";
+		}, 0);*/
 
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 Application::~Application()
 {
-    glfwTerminate();
+	glfwTerminate();
 }
 
 unsigned Application::ParseArguments(int argc, char** argv)
 {
-    return 1;
+	return 1;
 }
 
 unsigned Application::Init()
 {
-    // Print OpenGL data
-    std::cout << "Vendor: " << glGetString(GL_VENDOR) << "\n";
-    std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
-    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
+	// Print OpenGL data
+	std::cout << "Vendor: " << glGetString(GL_VENDOR) << "\n";
+	std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
+	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
 
-    shaderCube = std::make_shared<Shader>(std::string(SHADERS_DIR) + "model.vert",
-                                          std::string(SHADERS_DIR) + "model.frag");
+	shaderCube = std::make_shared<Shader>(std::string(SHADERS_DIR) + "model.vert",
+		std::string(SHADERS_DIR) + "model.frag");
 
-    shaderWall = std::make_shared<Shader>(std::string(SHADERS_DIR) + "model.vert",
-                                          std::string(SHADERS_DIR) + "model.frag");
+	shaderWall = std::make_shared<Shader>(std::string(SHADERS_DIR) + "model.vert",
+		std::string(SHADERS_DIR) + "model.frag");
 
-    shaderBlock = std::make_shared<Shader>(std::string(SHADERS_DIR) + "model.vert",
-                                           std::string(SHADERS_DIR) + "model.frag");
-
-
-    camera = PerspectiveCamera();
-    camera.SetFrustrum(PerspectiveCamera::Frustrum(45.0f, width, height, 0.1f, 100.0f));
+	shaderBlock = std::make_shared<Shader>(std::string(SHADERS_DIR) + "model.vert",
+		std::string(SHADERS_DIR) + "model.frag");
 
 
-    TextureManager::GetInstance()->LoadTexture2DRGBA("cube_texture",
-                                                     std::string(TEXTURES_DIR) + "cube_texture.png",
-                                                     0, true);
-
-    TextureManager::GetInstance()->LoadTexture2DRGBA("floor_texture",
-                                                     std::string(TEXTURES_DIR) + "marble.jpg",
-                                                     1, false);
+	camera = PerspectiveCamera();
+	camera.SetFrustrum(PerspectiveCamera::Frustrum(45.0f, width, height, 0.1f, 100.0f));
 
 
-    shaderCube->Bind();
-    shaderCube->UploadUniformInt("mode", 0);
-    shaderCube->Unbind();
+	TextureManager::GetInstance()->LoadTexture2DRGBA("cube_texture",
+		std::string(TEXTURES_DIR) + "cube_texture.png",
+		0, true);
 
-    shaderWall->Bind();
-    shaderWall->UploadUniformInt("mode", 1);
-    shaderWall->Unbind();
+	TextureManager::GetInstance()->LoadTexture2DRGBA("floor_texture",
+		std::string(TEXTURES_DIR) + "marble.jpg",
+		1, false);
 
-    shaderBlock->Bind();
-    shaderBlock->UploadUniformInt("mode", 1);
-    shaderBlock->Unbind();
-    
-    game = new Blockout();
-    scene = new Scene();
 
-    game->Init();
-    scene->Generate();
+	shaderCube->Bind();
+	shaderCube->UploadUniformInt("mode", 0);
+	shaderCube->Unbind();
 
-    srand(time(NULL));
-    return 1;
+	shaderWall->Bind();
+	shaderWall->UploadUniformInt("mode", 1);
+	shaderWall->Unbind();
+
+	shaderBlock->Bind();
+	shaderBlock->UploadUniformInt("mode", 1);
+	shaderBlock->Unbind();
+
+	game = new Blockout();
+	scene = new Scene();
+
+	game->Init();
+	scene->Generate();
+
+	srand(time(NULL));
+	return 1;
 }
 
 unsigned Application::Run()
 {
-    // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
+	// Main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
 
-        currentFrame = static_cast<float>(glfwGetTime());
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
+		currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 
-        currentTime = currentFrame - startFrame;
+		currentTime = currentFrame - startFrame;
+
+		Input(window);
+		game->Update(currentTime, startFrame);
+		PhongLighting::Update(deltaTime);
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glm::mat4 model = glm::mat4(1.0f);
+		const glm::mat4 projection = camera.GetProjectionMatrix();
+		const glm::mat4 view = camera.GetViewMatrix();
+
+		PhongLighting::SetLighting(*shaderWall, camera.GetPosition());
+		PhongLighting::SetLighting(*shaderBlock, camera.GetPosition());
 
 
-        Input(window);
-        game->Update(currentTime, startFrame);
-        PhongLighting::Update(deltaTime);
+		game->Render(*shaderBlock, projection, view);
+		game->RenderCubeMovement(*shaderCube, projection, view);
+		scene->Render(*shaderWall, projection, view);
 
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
 
-        glm::mat4 model = glm::mat4(1.0f);
-        const glm::mat4 projection = camera.GetProjectionMatrix();
-        const glm::mat4 view = camera.GetViewMatrix();
-
-        PhongLighting::SetLighting(*shaderWall, camera.GetPosition());
-        PhongLighting::SetLighting(*shaderBlock, camera.GetPosition());
-
-
-        game->Render(*shaderBlock, projection, view);
-        game->RenderCubeMovement(*shaderCube, projection, view);
-        scene->Render(*shaderWall, projection, view);
-
-        glfwSwapBuffers(window);
-    }
-
-    return 1;
+	return 1;
 }
 
 void Application::InputCallback()
 {
-    key_input = [&](int key, int sancode, int action, int mods)
-    {
-        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-        {
-            game->MoveBlock();
-        }
+	key_input = [&](int key, int sancode, int action, int mods)
+	{
+		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		{
+			game->MoveBlock();
+		}
 
-        if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
-        {
-            game->MoveBlock(0, -1, 0);
-        }
+		if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+		{
+			game->MoveBlock(0, -1, 0);
+		}
 
-        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-        {
-            game->MoveBlock(-1, 0, 0);
-        }
+		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		{
+			game->MoveBlock(-1, 0, 0);
+		}
 
-        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-        {
-            game->MoveBlock(1, 0, 0);
-        }
+		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		{
+			game->MoveBlock(1, 0, 0);
+		}
 
-        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-        {
-            game->MoveBlock(0, 0, -1);
-        }
+		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		{
+			game->MoveBlock(0, 0, -1);
+		}
 
-        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-        {
-            game->MoveBlock(0, 0, 1);
-        }
-        if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
-        {
-            // isBinding = !isBinding;
-            // shader->UploadUniformBool("isBinding", isBinding);
-        }
-    };
+		if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		{
+			game->MoveBlock(0, 0, 1);
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+		{
+			shaderCube->Bind();
+			shaderCube->UploadUniformInt("mode", 0);
+			shaderCube->Unbind();
+
+			shaderWall->Bind();
+			shaderWall->UploadUniformInt("mode", 3);
+			shaderWall->Unbind();
+
+			shaderBlock->Bind();
+			shaderBlock->UploadUniformInt("mode", 3);
+			shaderBlock->Unbind();
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+		{
+			shaderCube->Bind();
+			shaderCube->UploadUniformInt("mode", 0);
+			shaderCube->Unbind();
+
+			shaderWall->Bind();
+			shaderWall->UploadUniformInt("mode", 1);
+			shaderWall->Unbind();
+
+			shaderBlock->Bind();
+			shaderBlock->UploadUniformInt("mode", 1);
+			shaderBlock->Unbind();
+		}
+	};
 }
 
 void Application::Input(GLFWwindow* window)
 {
-    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
