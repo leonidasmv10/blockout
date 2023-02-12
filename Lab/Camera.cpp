51890a86e6c4231a755eafc641ad75ec43a2215b
Yordy Leonidas MV <yordy.lmv.2000@gmail.com>
2023-02-13 00:54:01 +0100

    remove branch

--- Lab/Camera.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "Camera.h"
-
-void Camera::RecalculateMatrix()
-{
-    
-}
--- Lab/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Application.h"
-
-int main(int argc, char *argv[])
-{
-    Application application("BlockOut", "1.0");
-    application.ParseArguments(argc, argv);
-    application.Init();
-    return application.Run();
-}
